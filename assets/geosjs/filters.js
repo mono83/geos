1e59e9c3be77d97d9d4801c93b9c9a9b5160a5fe
Stanislav Strebul <s.strebul@gmail.com>
2019-07-08 11:42:51 +0300

    Added dynamic filter by app name

--- assets/geosjs/filters.js
+++ assets/geosjs/filters.js
@@ -70,21 +70,9 @@ filters.push(new Filter("Trace", function (e) {
 filters.push(new Filter("Debug", function (e) {
     return e.getLevel() === "debug"
 }));
-filters.push(new Filter("[Java] MySQLTaskRepository garbage", function (e) {
-    return e.getLevel() === "debug" && e.data.name === "MySQLTaskRepository" && e.data.pattern === "No data received in :elapsed for :sql";
+filters.push(new Filter("Info", function (e) {
+    return e.getLevel() === "info"
 }));
-filters.push(new Filter("[Java] Task info", function (e) {
-    return e.getLevel() === "info" && e.data.name === "TaskBasedRpc"
-        && (e.data.pattern === "Incoming RPC request to :route" || e.data.pattern === "Task :taskId released with nextAt :next");
-}));
-filters.push(new Filter("[Java] Maintainer events", function (e) {
-    var level = e.getLevel();
-    return e.getLogger() === "maintain" && (level === "trace" || level === "debug" || level === "info");
-}));
-filters.push(new Filter("[AF] Reporting sync garbage", function (e) {
-    return e.getLevel() === "info" && e.data.name === "reportingSync" && e.data.pattern === "Handling incoming request on :path";
-}));
-
 
 if (window.GEOS) {
     window.GEOS._filters = filters;
